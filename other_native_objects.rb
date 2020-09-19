f90c01950e4c146e12ee0d8dbc1434cc1961b2a1
Ben <ben.dalton@madetech.com>
2020-09-19 18:28:06 +0100

    start time section 4.2

---
+++ other_native_objects.rb
@@ -0,0 +1,23 @@
+# puts Math::PI
+
+# puts Math.sqrt(81)
+
+# puts Math.cos(2*Math::PI)
+
+# puts Math.log(Math::E)
+
+# puts Math.log(10)
+
+# puts Math.log10(1)
+# puts Math.log10(10)
+# puts Math.log(1000)
+
+# puts 2**3
+
+# a = "1.24e6".to_f
+# puts a.to_s
+
+# puts Math.sqrt(10)
+
+# tau = 2 * Math::PI
+# puts tau
