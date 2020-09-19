22305a5ea6a0125cc5e798c494ab07105cddeea0
Ben <ben.dalton@madetech.com>
2020-09-19 17:49:23 +0100

    finish arrays section

---
+++ arrays.rb
@@ -0,0 +1,53 @@
+
+# a = "A man, a plan, a canal, Panama".split(", ")
+# p a
+
+# for i in "honey badger".split("")
+#     p i
+# end
+
+# new_list = "honey badger".split("")
+# new_list.each { |ch| p ch.upcase}
+
+# a = [42, 8, 17, 99]
+# # p a.slice(2,2)
+# # p a[2,2]
+# p a[1..3]
+
+# test = ('a'..'z').to_a
+# test.each { |ch| p ch.upcase }
+
+# numbers = (1..10).to_a
+# # p numbers
+# p numbers[2..numbers.length - 3]
+# p numbers[2..-3]
+
+# strings = "ant bat cat"
+# p strings[4..6]
+
+# first_13 = ('a'..'z').to_a[0..13]
+# p first_13
+
+# a = [42, 8, 17, 99]
+# # p a.sort!
+# # p a.reverse!
+# # p a
+
+# # a.push(6)
+# # p a
+# # p a.pop
+# # p a
+
+# a.pop
+# a << "ant" << "bat" << "cat"
+# p a
+
+# a = ["ant", "bat", "cat"]
+# a.unshift(1)
+# p a
+
+# for i in 0..a.length
+#     puts a[i]
+# end
+
+# a.reverse.each { |i| puts i}
