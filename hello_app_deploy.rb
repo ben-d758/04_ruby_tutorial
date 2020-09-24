d983dfd90c97245017c327d7f725d1f92a3f375c
Ben <ben.dalton@madetech.com>
2020-09-24 16:02:05 +0100

    configured hello_app_deploy.rb to print 'hello, world and the day'

--- hello_app_deploy.rb
+++ hello_app_deploy.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 
 get '/' do
-    'hello, world!'
-end
+    DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday",
+                "Thursday", "Friday", "Saturday"]
+    dayname = DAYNAMES[Time.now.wday]
+    "Hello, world! Happy #{dayname}."
+  end
