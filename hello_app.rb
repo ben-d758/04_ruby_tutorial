8bf921abe9271884b0750a8f7f70cc2658083050
Ben <ben.dalton@madetech.com>
2020-09-24 16:47:33 +0100

    pull DAYNAMES out of 'get'

--- hello_app.rb
+++ hello_app.rb
@@ -1,8 +1,9 @@
 require 'sinatra'
 
+DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday",
+    "Thursday", "Friday", "Saturday"]
+    
 get '/' do
-    DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday",
-                "Thursday", "Friday", "Saturday"]
     dayname = DAYNAMES[Time.now.wday]
     "Hello, world! Happy #{dayname}."
 end
