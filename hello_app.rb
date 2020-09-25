1ec748f19e62da0fec86c3e98aeb9884a3444d8b
Ben <ben.dalton@madetech.com>
2020-09-25 10:27:36 +0100

    update hello_app.rb with simple day_of_the_week funciton

--- hello_app.rb
+++ hello_app.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
     
+def day_of_the_week(time)
+    Date::DAYNAMES[time.wday]
+end
+
 get '/' do
-    dayname = Date::DAYNAMES[Time.now.wday]
-    "Hello, world! Happy #{dayname}."
+    "Hello, world! Happy #{day_of_the_week(Time.now)}."
 end
