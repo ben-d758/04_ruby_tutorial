ddeed712b35e07d7fab448d6658027d10e943170
Ben <ben.dalton@madetech.com>
2020-09-25 10:59:26 +0100

    add greeting function to be called in hello_app.rb

--- functions_blocks/day.rb
+++ functions_blocks/day.rb
@@ -3,3 +3,7 @@ require 'date'
 def day_of_the_week(time)
     Date::DAYNAMES[time.wday]
 end
+
+def greeting(time)
+    "Hello! Today is #{day_of_the_week(time)}"
+end
--- hello_app.rb
+++ hello_app.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 require_relative './functions_blocks/day'
 
-get '/' do
-    "Hello, world! Happy #{day_of_the_week(Time.now)}—now from a file!"
-end
+# get '/' do
+#     "Hello, world! Happy #{day_of_the_week(Time.now)}—now from a file!"
+# end
+
+get '/' do 
+    greeting(Time.now)
+end 
