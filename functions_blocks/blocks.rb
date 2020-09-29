c132f1bb1dc008f5e479538089e60840c5a52317
Ben <ben.dalton@madetech.com>
2020-09-29 21:19:37 +0100

    add sandwich method with block

--- functions_blocks/blocks.rb
+++ functions_blocks/blocks.rb
@@ -1,1 +1,15 @@
-(1..5).each { |i| puts 2**i }
+# (1..5).each { |i| puts 2**i }
+
+
+# Every Ruby method can take a block and we can invoke the block using the 'yield' keyword
+
+def sandwich
+    puts "top bread"
+    yield
+    puts "bottom bread"
+end
+
+sandwich do 
+    puts "lettuce, tomato, cheese"
+end
+
