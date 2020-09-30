0cd5b3f3c38de66c875dcebb9b808f721971e691
Ben <ben.dalton@madetech.com>
2020-09-30 19:31:05 +0100

    Imperative version of url builder

---
+++ functional_programming/functional.rb
@@ -0,0 +1,21 @@
+# 'Map' - allows you to map a function over an array of elements. A powerful alternative to looping.
+
+# Making URL-appropriate strings from an array using a block to loop
+
+states = ["Kansas", "Nebraska", "North Dakota", "South Dakota"]
+
+# Imperative version
+
+def imperative_urls(states)
+    urls = []
+    states.each do |state|
+        urls << state.downcase.split.join("-")
+    end
+    urls
+end
+
+imperative_urls(states) # ["kansas", "nebraska", "north-dakota", "south-dakota"]
+
+
+
+
