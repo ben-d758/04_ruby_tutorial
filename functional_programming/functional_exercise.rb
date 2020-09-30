aaf1a1227a303db5d0fd4602495867c0789ec143
Ben <ben.dalton@madetech.com>
2020-09-30 19:37:05 +0100

    exercise: imperative url function version w/o map

---
+++ functional_programming/functional_exercise.rb
@@ -0,0 +1,19 @@
+# Using 'map', write a function that takes in the 'states' variable and returns an array of URLs of the form
+    # "https://example.com/<urlified form>"
+
+states = ["Kansas", "Nebraska", "North Dakota", "South Dakota"]
+
+# Imperative version
+
+def imperative_url(states)
+    urls = []
+    states.each do |state|
+        urls << "https://example.com/#{state.downcase.split.join("-")}"
+    end
+    urls
+end
+
+imperative_url(states)
+# ["https://example.com/kansas", "https://example.com/nebraska", "https://example.com/north-dakota", "https://example.com/south-dakota"]
+
+
