ca348b05c72deeac10f1dc668f13a3b3f5451c85
Ben <ben.dalton@madetech.com>
2020-09-30 19:31:50 +0100

    Using map introduction

--- functional_programming/functional.rb
+++ functional_programming/functional.rb
@@ -17,5 +17,13 @@ end
 imperative_urls(states) # ["kansas", "nebraska", "north-dakota", "south-dakota"]
 
 
+# Using 'Map'
 
+#  Square every element in an array of numbers
+
+squared_nums = [1, 2, 3, 4].map { |n| n*n }
+squared_nums # [1, 4, 9, 16]
+
+lowercase_names = ["ALICE", "BOB", "CHARLIE"].map { |name| name.downcase }
+lowercase_names # ["alice", "bob", "charlie"]
 
