05145476a30f00fab7750a7ac5269fb63b309d6f
Ben <ben.dalton@madetech.com>
2020-10-08 19:04:20 +0100

    moved method into class

--- classes/palindrome.rb
+++ classes/palindrome.rb
@@ -6,19 +6,11 @@ class Phrase
     end
 
     def palindrome?
-        processed_content = string.downcase
+        processed_content = self.content.downcase
         processed_content == processed_content.reverse
     end
 end
 
-# phrase = Phrase.new("Madam, I'm Adam.")
-# phrase.content # "Madam, I'm Adam."
-
-# phrase.content = "Able was I, ere I saw Elba."
-# phrase.content # "Able was I, ere I saw Elba."
-
 phrase = Phrase.new("Racecar")
-puts phrase.content # "Racecar"
-
-palindrome?(phrase.content) # true
+puts phrase.palindrome? # true
 
