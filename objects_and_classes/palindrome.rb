5325d817ce65a9df4a10971f8bbfb1e723fa28ed
Ben <ben.dalton@madetech.com>
2020-10-08 19:26:55 +0100

    add louder method

--- objects_and_classes/palindrome.rb
+++ objects_and_classes/palindrome.rb
@@ -9,8 +9,14 @@ class Phrase
         processed_content = self.content.downcase
         processed_content == processed_content.reverse
     end
+
+    def louder
+        shout = self.content.upcase
+    end
 end
 
-phrase = Phrase.new("Racecar")
-puts phrase.palindrome? # true
+# phrase = Phrase.new("Racecar")
+# puts phrase.palindrome? # true
 
+p = Phrase.new("yo adrian!")
+puts p.louder
